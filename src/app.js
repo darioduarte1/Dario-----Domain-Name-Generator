96665db26d890a3e5c0f9178d38edf3508687570
darioduarte1 <darioduarte1991@gmail.com>
2024-05-28 13:59:57 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { Generator } from "webpack";
 
 let firstWords = ["wonderful", "great", "fantastic", "incredible"];
 let secondWords = ["car", "truck", "bike", "bus"];
@@ -12,26 +11,33 @@ let thirdWords = ["111", "333", "555", "777"];
 let domainWords = ["com", "es", "pt", "fr"];
 
 function generatorAllDomain() {
+  let myArray = [];
+
   for (let firstElement of firstWords) {
     for (let secondElement of secondWords) {
       for (let thirdElement of thirdWords) {
         for (let domainElement of domainWords) {
-          console.log(
-            firstElement + secondElement + thirdElement + "." + domainElement
+          myArray.push(
+            " " +
+              firstElement +
+              secondElement +
+              thirdElement +
+              "." +
+              domainElement
           );
         }
       }
     }
   }
+  return myArray;
 }
-
 generatorAllDomain();
 
-// Función para asignar el dominio al elemento HTML
+// Funcao para o dominio html
 function assignDomain() {
   const domainElement = document.querySelector("#domain");
-  domainElement.innerHTML = generatorAllDomain(); // Asignar el dominio generado
+  domainElement.innerHTML = generatorAllDomain(); // introduzir o dominio gerado
 }
 
-// Asegurar que el código se ejecute después de que la página se cargue
+// Asegurar que o codigo se execute depois da pagina carregada
 document.addEventListener("DOMContentLoaded", assignDomain);
